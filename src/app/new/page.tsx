920866cbf6ab81ff7aace4372e69ff9b15f1ab72
ellioats <emwcurrie@gmail.com>
2025-05-14 10:46:21 -0400

    implementing text input with mui #wip

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,9 +1,17 @@
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+
 export default function Page() {
 
-    
     return (
         <>
-            <h1 className="">Create new Post</h1>
+            <h1 className="text-2xl font-bold">Create new Post</h1>
+
+            <Box>
+                <div className="border-2 decoration-white bg-transparent text-white p-2 border-solid border-white">
+                    <TextField id="standard-basic" label="test" variant="standard"></TextField>
+                </div>
+            </Box>
 
         </>
     );
