63c9f9f558e006da30f8e371cdb51cdfd861a13f
ellioats <emwcurrie@gmail.com>
2025-05-14 16:40:57 -0400

    added basic journal entry ui #impl

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -8,8 +8,22 @@ export default function Page() {
             <h1 className="text-2xl font-bold">Create new Post</h1>
 
             <Box>
-                <div className="border-2 decoration-white bg-transparent text-white p-2 border-solid border-white">
-                    <TextField id="standard-basic" label="test" variant="standard"></TextField>
+                <div className="flex flex-row">
+                    <div className="inline-flex decoration-white bg-transparent text-white">
+                        <TextField id="standard-basic" label="Title" variant="standard" sx={{ width: '120px', top: '-20px', left: '10px' }}></TextField>
+                    </div>
+                </div>
+
+                <div>
+                    <TextField
+                        id="multiline-static"
+                        label="Write here!"
+                        multiline
+                        rows={16}
+                        defaultValue="Once upon a time..."
+                        variant="standard"
+                        sx={{ width: '100%', height: '100%' }}
+                    />
                 </div>
             </Box>
 
