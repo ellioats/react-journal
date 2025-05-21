71ce439881357a9a6fbea2215a3377bee87773b3
Elliot <emwcurrie@gmail.com>
2025-05-21 04:06:07 -0400

    added card objects and conditional rendering of titles based on length

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,12 +1,36 @@
-import Header from "../../../components/global/header";
+"use client"
+import { Post } from "../../../types/PostType";
+import { useEffect, useState } from 'react';
+import PostCard from "../../../components/cards/PostCard";
 
-export default function Page() {
-    
-    
-    return (
-        <>
-            <h1>View page</h1>
-        </>
-    );
+export default function Page({ }) {
+
+  const [data, setData] = useState<Post[]>([]);
+
+  // fetch posts
+  useEffect(() => {
+    fetch('entries.json')
+      .then((res) => res.json())
+      .then((json) => setData(json))
+  })
+
+  // if data is null 
+  if (!data) {
+    return (<p>Loading</p>);
+  }
+
+  return (
+    <>
+      <h1>View page</h1>
+      <hr />
+      <br />
+
+      <div className="border-2 m-5 p-2 flex flex-wrap gap-4">
+        {data.map((entries: Post, index) => {
+          return <PostCard entry={entries} key={index} />
+        })}
+      </div>
+    </>
+  );
 
 }
